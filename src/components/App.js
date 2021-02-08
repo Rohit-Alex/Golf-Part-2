0c7237c54076da04a5dac167d0fa2129df2ce66d
Rohit <rohit5012000@gmail.com>
2021-02-09 00:05:52 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,110 @@
-import React, { Component, useState } from "react";
+// import React, { Component, useState } from "react";
+// import "../styles/App.css";
+
+// const App = () => {
+//   const [renderBall, setRenderBall] = useState(false);
+//   const [x, setX] = useState(0);
+//   const [y, setY] = useState(0);
+//   const [ballPosition,setBallPosition] = useState({
+//     left: "0px",
+//     top: "0px",
+//   });
+//   const reset = () => {};
+//   const renderChoice = () => {};
+
+//   return (
+//     <div className="playground">
+//       <button onClick={reset} className="reset">
+//         Reset
+//       </button>
+//       {renderChoice()}
+//     </div>
+//   );
+// };
+
+// export default App;
+
+import React, { useEffect, useState } from "react";
 import "../styles/App.css";
 
 const App = () => {
   const [renderBall, setRenderBall] = useState(false);
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
-  const [ballPosition,setBallPosition] = useState({
-    left: "0px",
-    top: "0px",
+  // const [posi, setPosi] = useState(0);
+  const [ballPosition, setBallPosition] = useState({
+    left: 0,
+    top: 0,
+    position: "absolute",
   });
-  const reset = () => {};
-  const renderChoice = () => {};
 
-  return (
-    <div className="playground">
-      <button onClick={reset} className="reset">
-        Reset
-      </button>
-      {renderChoice()}
-    </div>
-  );
+  const handler = (e) => {
+    switch (e.keyCode) {
+      case 39:
+        setBallPosition({
+          left: ballPosition.left + 5,
+          top: ballPosition.top,
+          position: "absolute",
+        });
+        break;
+      case 40:
+        setBallPosition({
+          left: ballPosition.left,
+          top: ballPosition.top + 5,
+          position: "absolute",
+        });
+        break;
+      case 37:
+        setBallPosition({
+          left: ballPosition.left - 5,
+          top: ballPosition.top,
+          position: "absolute",
+        });
+        break;
+      case 38:
+        setBallPosition({
+          left: ballPosition.left,
+          top: ballPosition.top - 5,
+          position: "absolute",
+        });
+        break;
+      default:
+        break;
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", handler);
+    return () => document.removeEventListener("keydown", handler);
+  }, [ballPosition]);
+
+  const buttonClickHandler = () => {
+    setRenderBall(true);
+  };
+
+const reset = () => {
+  setBallPosition({
+    left: 0,
+    top: 0,
+    position: "absolute",
+  });
 };
+
+  const renderChoice = () => {
+    if (renderBall) {
+      return (
+        <div className="ball"
+          style={ballPosition}>
+        </div>
+      );
+    } 
+    else {
+      return <button onClick={buttonClickHandler}> Click For One Ball </button>;
+    }
+  };
+
+  return <div className = "playground">
+       <button onClick={reset} className="reset">Reset
+       </button> {renderChoice()}
+       </div>
+  };
 
 export default App;
